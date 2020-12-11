994c47bae3dc0a3c2069be29f462e7108cf57e62
jorge220997 <jorge220997@gmail.com>
2020-12-11 01:51:53 -0500

    Defined MyUser model and added boilerplates for MyUser controller and service

---
+++ src/PackingListApp/Controllers/MyUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PackingList.Core.Queries;
+using PackingListApp.Interfaces;
+using PackingListApp.Models;
+
+namespace PackingList.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class MyUserController : ControllerBase {   
+    }
+}
---
+++ src/PackingListApp/Models/MyUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace PackingListApp.Models
+{
+    public class MyUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public override string ToString() => JsonConvert.SerializeObject(this);
+    }
+}
