10ce89c1778ef7533648d566bd1e0390e248e153
Isabella Hernández García <isabellahg@users.noreply.github.com>
2020-12-13 23:16:13 -0800

    Update body.tsx to fix layout issue caused by typo

--- src/PackingListApp/ClientApp/src/pages/User/body.tsx
+++ src/PackingListApp/ClientApp/src/pages/User/body.tsx
@@ -1,51 +1,50 @@
+﻿import React, { FC, ReactElement } from "react";
 import { Col, Input, Row } from "antd";
 import Form, { GetFieldDecoratorOptions } from "antd/lib/form/Form";
 import FormItem from "antd/lib/form/FormItem";
-import React, { FC, ReactElement } from "react";
 import { NewUser, User } from "../../stores/userStore";
 import { nameof } from "../../utils/object";
 import { FormBodyProps } from "../common/NewItemView";
 
 const UserFormBody: FC<FormBodyProps<NewUser>> = ({
-    getFieldDecorator,
-    item,
-    onSave,
+  getFieldDecorator,
+  item,
+  onSave
 }): ReactElement => {
-    return (
-        <Form
-            id="modaForm"
-            onSubmit={() => {
-                if (onSave) {
-                    onSave();
-                }
-            }}
-        >
-            <Row gutter={24}>
-                <Col span={6}>
-                    <FormItem label={"First Name"}>
-                        {getFieldDecorator(nameof<NewUser>("firstName"), {
-                            initialValue: (item as NewUser).firstName,
-                        })(<Input />)}
-                    </FormItem>
-                </Col>
-                <Col span={6}>
-                    <FormItem label={"Last Name"}>
-                        {getFieldDecorator(nameof<NewUser>("lastName"), {
-                            initialValue: (item as NewUser).lastName,
-                        })(<Input />)}
-                    </FormItem>
-                </Col>
-                <Col span={12}>
-                    s
-                    <FormItem label={"Address"}>
-                        {getFieldDecorator(nameof<NewUser>("address"), {
-                            initialValue: (item as NewUser).address,
-                        })(<Input />)}
-                    </FormItem>
-                </Col>
-            </Row>
-        </Form>
-    );
+  return (
+    <Form
+      id="modaForm"
+      onSubmit={() => {
+        if (onSave) {
+          onSave();
+        }
+      }}
+    >
+      <Row gutter={24}>
+        <Col span={12}>
+          <FormItem label={"First Name"}>
+            {getFieldDecorator(nameof<NewUser>("firstName"), {
+              initialValue: (item as NewUser).firstName
+            })(<Input />)}
+          </FormItem>
+        </Col>
+        <Col span={12}>
+          <FormItem label={"Last Name"}>
+            {getFieldDecorator(nameof<NewUser>("lastName"), {
+              initialValue: (item as NewUser).lastName
+            })(<Input />)}
+          </FormItem>
+        </Col>
+        <Col span={24}>
+          <FormItem label={"Address"}>
+            {getFieldDecorator(nameof<NewUser>("address"), {
+              initialValue: (item as NewUser).address
+            })(<Input />)}
+          </FormItem>
+        </Col>
+      </Row>
+    </Form>
+  );
 };
 
 export default UserFormBody;
