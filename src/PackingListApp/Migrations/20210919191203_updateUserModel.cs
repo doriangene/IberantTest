957cb76a15127963151a299cf885b2b0fb336fda
Joel David <jdavidhc1710@gmail.com>
2021-09-19 15:12:35 -0400

    Added Latest Migration

---
+++ src/PackingListApp/Migrations/20210919191203_updateUserModel.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PackingListApp.Migrations
+{
+    public partial class updateUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "UserModels",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "UserModels",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AdminType",
+                table: "UserModels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "UserModels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "UserModels");
+
+            migrationBuilder.DropColumn(
+                name: "AdminType",
+                table: "UserModels");
+
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "UserModels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "UserModels",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+        }
+    }
+}
