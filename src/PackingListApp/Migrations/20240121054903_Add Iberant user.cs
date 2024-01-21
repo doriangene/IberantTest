9568d0b1f0be3b2beaac27366f256829faca6cdd
Daniela <danielarodriguezcepero@gmail.com>
2024-01-21 01:06:45 -0500

    Add user Iberant.

---
+++ src/PackingListApp/Migrations/20240121054903_Add Iberant user.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PackingListApp.Migrations
+{
+    public partial class AddIberantuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO UserModels(Name, LastName, Direction, IsAdmin, AdminType) VALUES(N'Iberant', N'Iberant', N'Adding Iberant user in UserModels', N'0', N'0')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
