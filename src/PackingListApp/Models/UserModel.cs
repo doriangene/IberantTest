7018631c4602cc8609a58c039567ab433e2fa030
BryanMachin <bryan.machin@bbconsult.co.uk>
2025-04-04 05:37:49 -0400

    MaxLengthForAddress Migration

--- src/PackingListApp/Models/UserModel.cs
+++ src/PackingListApp/Models/UserModel.cs
@@ -9,6 +9,8 @@ namespace PackingListApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+
+        [MaxLength(10)]
         public string Address { get; set; }
         public bool IsAdmin { get; set; }
         public AdminType AdminType { get; set; }
