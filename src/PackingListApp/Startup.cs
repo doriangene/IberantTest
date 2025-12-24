88a3ca1290a20a5a317d275dde71d58eb0773c50
Phann020126 <daniel020126@gmail.com>
2025-12-24 18:15:53 -0500

    fix: running with .net 5

--- src/PackingListApp/Startup.cs
+++ src/PackingListApp/Startup.cs
@@ -30,7 +30,7 @@ namespace PackingListApp
         {
             services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<TestContext>(options => options.UseSqlServer(Configuration["TestContext:ConnectionString"]));
+            services.AddDbContext<TestContext>(options => options.UseSqlServer(Configuration["TestContext:ConnectionStrings:DefaultConnection"]));
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
