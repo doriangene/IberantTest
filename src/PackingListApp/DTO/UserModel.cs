cb31708fee9a78c4c1cbcbe44cbf31ac780a0d99
Phann020126 <daniel020126@gmail.com>
2025-12-25 15:45:33 -0500

    e1: backend

---
+++ src/PackingListApp/DTO/UserModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackingListApp.Models
+{
+    public class NewUserModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ src/PackingListApp/Migrations/20251225000000_init.Designer.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using PackingListApp.EntityFramework;
+
+namespace PackingListApp.Migrations
+{
+    [DbContext(typeof(TestContext))]
+    [Migration("20251225000000_init")]
+    partial class AddUserTable
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128)
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("PackingListApp.Models.UserModel", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<string>("Address");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("UserModels");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}
