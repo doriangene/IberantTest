701e774e65f05d5c59a899a0169f9412105bdcc9
Phann020126 <daniel020126@gmail.com>
2025-12-27 18:17:18 -0500

    e6: add occupation - migrations

--- src/PackingListApp/Migrations/20251227230644_AddOccupationFKToUser.Designer.cs
+++ src/PackingListApp/Migrations/20251227230644_AddOccupationFKToUser.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PackingListApp.EntityFramework;
 
 namespace PackingListApp.Migrations
 {
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot
+    [Migration("20251227230644_AddOccupationFKToUser")]
+    partial class AddOccupationFKToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PackingListApp/Migrations/20251227230644_AddOccupationFKToUser.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PackingListApp.Migrations
+{
+    public partial class AddOccupationFKToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OccupationId",
+                table: "UserModels",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserModels_OccupationId",
+                table: "UserModels",
+                column: "OccupationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserModels_OccupationModels_OccupationId",
+                table: "UserModels",
+                column: "OccupationId",
+                principalTable: "OccupationModels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserModels_OccupationModels_OccupationId",
+                table: "UserModels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserModels_OccupationId",
+                table: "UserModels");
+
+            migrationBuilder.DropColumn(
+                name: "OccupationId",
+                table: "UserModels");
+        }
+    }
+}
